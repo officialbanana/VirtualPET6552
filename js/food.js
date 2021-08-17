51011de59dad77898d2f15fd0f8dad54ebc6d505
officialbanana <62805568+officialbanana@users.noreply.github.com>
2021-08-17 08:35:12 -0500

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,41 @@
+
+class Food{
+   constructor() {
+       this.image = loadImage("Images/Milk.png");
+       this.foodStock=0;
+       this.lastFed;
+   } 
+   updateFoodStock(foodStock) {
+this.foodStock=foodStock
+   }
+   getFedTime(lastFed){
+     this.lastFed=lastFed;
+   }
+   getFoodStock(){
+return this.foodStock
+   }
+
+   deductFood() {
+if (this.foodStock > 0){
+this.foodStock=this.foodStock-1 
+}
+}
+   
+   display(){
+   var x=80
+   var y = 100;
+image(this.image,720,220, 70,70);
+ 
+    if(this.foodStock!=0){
+        for(var i=0;i<this.foodStock;i++){
+          if(i%10==0){
+            x=80;
+            y=y+50;
+          }
+          image(this.image,x,y,50,50);
+          x=x+30;
+        }
+   }
+
+   }
+}
